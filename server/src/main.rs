fdc56c3108c95bcbc7969d5ff6b8f91d07259f8c
Brian Schwind <brianmschwind@gmail.com>
2021-07-10 18:35:48 +0900

    Move the server over to bevy

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,126 +1,204 @@
-use laminar::{Config as NetworkConfig, ErrorKind, Packet, Socket, SocketEvent};
-use std::{collections::HashMap, net::SocketAddr, time::Duration};
+use crossbeam_channel::{Receiver, Sender};
+use laminar::{Config as NetworkConfig, Packet, Socket, SocketEvent};
+use simple_game::bevy::{
+    App, AppBuilder, Commands, CorePlugin, EventReader, EventWriter, FixedTimestep,
+    HeadlessBevyGame, IntoSystem, ResMut, SystemSet,
+};
+use std::{collections::HashMap, net::SocketAddr, thread::JoinHandle, time::Duration};
 use sus_common::{
-    network::{ClientToServer, FullGameStatePacket, NewPlayerPacket, ServerToClient},
-    Game, Player,
+    network::{
+        ClientToServer, ConnectPacket, FullGameStatePacket, NewPlayerPacket, PlayerInputPacket,
+        ServerToClient,
+    },
+    Player,
 };
 
 const BIND_ADDR: &str = "0.0.0.0:7600";
 const MAX_PLAYERS: usize = 16;
 
-fn main() -> Result<(), ErrorKind> {
-    let mut game = Game::new();
+struct SusServer {
+    _network_thread: JoinHandle<()>,
+    net_tx: Sender<laminar::Packet>,
+    net_rx: Receiver<SocketEvent>,
+    players: HashMap<SocketAddr, Player>,
+    id_counter: u16,
+}
 
+struct PlayerInput {
+    id: u16,
+    input: PlayerInputPacket,
+}
+
+struct NewPlayer {
+    addr: SocketAddr,
+    connect_packet: ConnectPacket,
+}
+
+impl HeadlessBevyGame for SusServer {
+    fn init_systems() -> AppBuilder {
+        let mut ecs_world_builder = App::build();
+
+        ecs_world_builder
+            .add_plugin(CorePlugin)
+            .add_event::<PlayerInput>()
+            .add_event::<NewPlayer>()
+            .add_startup_system(init.system())
+            .add_system_set(
+                SystemSet::new()
+                    .with_run_criteria(
+                        FixedTimestep::step(1.0 / Self::desired_fps() as f64)
+                            .with_label("game_timestep"),
+                    )
+                    .with_system(update_network.system()) // TOOD(bschwind) - Enforce ordering on network systems
+                    .with_system(handle_player_input.system())
+                    .with_system(handle_new_player.system()),
+            );
+
+        ecs_world_builder
+    }
+}
+
+fn init(mut commands: Commands) {
+    let mut socket = initialize_network();
+    let (net_tx, net_rx) = (socket.get_packet_sender(), socket.get_event_receiver());
+
+    let server = SusServer {
+        _network_thread: std::thread::spawn(move || socket.start_polling()),
+        net_rx,
+        net_tx,
+        players: HashMap::new(),
+        id_counter: 0,
+    };
+
+    commands.insert_resource(server);
+}
+
+fn initialize_network() -> Socket {
     // TODO(bschwind) - Remove this once we start having a steady flow of packets.
     let net_config = NetworkConfig {
         idle_connection_timeout: Duration::from_secs(5),
         heartbeat_interval: Some(Duration::from_secs(4)),
         ..NetworkConfig::default()
     };
-    let mut socket = Socket::bind_with_config(BIND_ADDR, net_config)?;
-    let (sender, receiver) = (socket.get_packet_sender(), socket.get_event_receiver());
-    let mut players: HashMap<SocketAddr, Player> = HashMap::new();
-    let _thread = std::thread::spawn(move || socket.start_polling());
 
-    // Game state
-    let mut id_counter: u16 = 0;
+    Socket::bind_with_config(BIND_ADDR, net_config).expect("Couldn't bind to server BIND_ADDR")
+}
 
-    loop {
-        if let Ok(event) = receiver.recv() {
-            match event {
-                SocketEvent::Packet(packet) => {
-                    let msg = packet.payload();
+fn handle_player_input(mut input_rx: EventReader<PlayerInput>) {
+    for event in input_rx.iter() {
+        println!("Player (id={}) sent input: {:?}", event.id, event.input);
+    }
+}
 
-                    if let Ok(decoded) = bincode::deserialize::<ClientToServer>(msg) {
-                        match decoded {
-                            ClientToServer::Connect(connect_packet) => {
-                                println!(
-                                    "{} (ip = {}) connected with game version {}",
-                                    connect_packet.name,
-                                    packet.addr(),
-                                    connect_packet.version
-                                );
+fn handle_new_player(mut server: ResMut<SusServer>, mut new_player_rx: EventReader<NewPlayer>) {
+    for new_player in new_player_rx.iter() {
+        let addr = new_player.addr;
+        let connect_packet = &new_player.connect_packet;
 
-                                if players.len() >= MAX_PLAYERS {
-                                    println!("Max players exceeded, not accepting");
-                                } else {
-                                    let new_player_id = id_counter;
-                                    let new_player_pos = (0, 0);
-                                    players.insert(
-                                        packet.addr(),
-                                        Player::new(&connect_packet.name, new_player_id),
-                                    );
-                                    id_counter += 1;
+        println!(
+            "{} (ip = {}) connected with game version {}",
+            connect_packet.name, addr, connect_packet.version
+        );
 
-                                    let reply = ServerToClient::ConnectAck;
-                                    sender
-                                        .send(Packet::reliable_ordered(
-                                            packet.addr(),
-                                            bincode::serialize(&reply).unwrap(),
-                                            None,
-                                        ))
-                                        .expect("Failed to send ConnectAck");
-                                    // Send all existing state to new client
-                                    let players_vec = players
-                                        .values()
-                                        .map(|p| NewPlayerPacket::new(p.name.clone(), p.id, p.pos))
-                                        .collect();
+        if server.players.len() >= MAX_PLAYERS {
+            println!("Max players exceeded, not accepting");
+        } else {
+            let new_player_id = server.id_counter;
+            let new_player_pos = (0, 0);
+            server.players.insert(addr, Player::new(&connect_packet.name, new_player_id));
+            server.id_counter += 1;
 
-                                    let full_state_packet = ServerToClient::FullGameState(
-                                        FullGameStatePacket::new(players_vec),
-                                    );
-                                    sender
-                                        .send(Packet::reliable_ordered(
-                                            packet.addr(),
-                                            bincode::serialize(&full_state_packet).unwrap(),
-                                            None,
-                                        ))
-                                        .expect("Failed to send ConnectAck");
+            let reply = ServerToClient::ConnectAck;
+            server
+                .net_tx
+                .send(Packet::reliable_ordered(addr, bincode::serialize(&reply).unwrap(), None))
+                .expect("Failed to send ConnectAck");
+            // Send all existing state to new client
+            let players_vec = server
+                .players
+                .values()
+                .map(|p| NewPlayerPacket::new(p.name.clone(), p.id, p.pos))
+                .collect();
 
-                                    // Tell all other players this one has connected
-                                    let new_player_packet =
-                                        ServerToClient::NewPlayer(NewPlayerPacket::new(
-                                            connect_packet.name,
-                                            new_player_id,
-                                            new_player_pos,
-                                        ));
-                                    for player_addr in players.keys() {
-                                        if *player_addr != packet.addr() {
-                                            sender
-                                                .send(Packet::reliable_ordered(
-                                                    *player_addr,
-                                                    bincode::serialize(&new_player_packet).unwrap(),
-                                                    None,
-                                                ))
-                                                .expect("Failed to send ConnectAck");
-                                        }
-                                    }
-                                }
-                            },
-                            ClientToServer::PlayerInput(input) => {
-                                if let Some(player) = players.get(&packet.addr()) {
-                                    // println!("Player {} (id={}) sent input: {:?}", player.name, player.id, input);
-                                }
-                            },
-                        }
-                    } else {
-                        println!("Received an invalid packet");
-                    }
-                },
-                SocketEvent::Timeout(addr) => {
-                    println!("Client timed out: {}", addr);
-                },
-                SocketEvent::Connect(addr) => {
-                    println!("Client connected: {}", addr);
-                },
-                SocketEvent::Disconnect(addr) => {
-                    if let Some(player) = players.remove(&addr) {
-                        println!("Player {} disconnected ({})", player.name, addr);
-                    } else {
-                        println!("Unknown player disconnected: {}", addr);
-                    }
-                },
+            let full_state_packet =
+                ServerToClient::FullGameState(FullGameStatePacket::new(players_vec));
+            server
+                .net_tx
+                .send(Packet::reliable_ordered(
+                    addr,
+                    bincode::serialize(&full_state_packet).unwrap(),
+                    None,
+                ))
+                .expect("Failed to send ConnectAck");
+
+            // Tell all other players this one has connected
+            let new_player_packet = ServerToClient::NewPlayer(NewPlayerPacket::new(
+                connect_packet.name.clone(), // TODO(bschwind) - can we get an EventReader iterator which gives owned values?
+                new_player_id,
+                new_player_pos,
+            ));
+            for player_addr in server.players.keys() {
+                if *player_addr != addr {
+                    server
+                        .net_tx
+                        .send(Packet::reliable_ordered(
+                            *player_addr,
+                            bincode::serialize(&new_player_packet).unwrap(),
+                            None,
+                        ))
+                        .expect("Failed to send ConnectAck");
+                }
             }
         }
     }
+}
+
+fn update_network(
+    mut server: ResMut<SusServer>,
+    mut new_player_tx: EventWriter<NewPlayer>,
+    mut input_tx: EventWriter<PlayerInput>,
+) {
+    if let Ok(event) = server.net_rx.recv() {
+        match event {
+            SocketEvent::Packet(packet) => {
+                let msg = packet.payload();
+
+                if let Ok(decoded) = bincode::deserialize::<ClientToServer>(msg) {
+                    match decoded {
+                        ClientToServer::Connect(connect_packet) => {
+                            new_player_tx.send(NewPlayer { addr: packet.addr(), connect_packet });
+                        },
+                        ClientToServer::PlayerInput(input) => {
+                            if let Some(player) = server.players.get(&packet.addr()) {
+                                input_tx.send(PlayerInput { id: player.id, input });
+                            }
+                        },
+                    }
+                } else {
+                    println!("Received an invalid packet");
+                }
+            },
+            SocketEvent::Timeout(addr) => {
+                if let Some(player) = server.players.get(&addr) {
+                    println!("{} ({}) timed out", player.name, addr);
+                } else {
+                    println!("Unknown player timed out: {}", addr);
+                }
+            },
+            SocketEvent::Connect(addr) => {
+                println!("Client connected: {}", addr);
+            },
+            SocketEvent::Disconnect(addr) => {
+                if let Some(player) = server.players.remove(&addr) {
+                    println!("Player {} disconnected ({})", player.name, addr);
+                } else {
+                    println!("Unknown player disconnected: {}", addr);
+                }
+            },
+        }
+    }
+}
+
+fn main() {
+    simple_game::bevy::run_headless_bevy_game::<SusServer>();
 }
